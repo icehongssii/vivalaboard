923016cbb9a8282b563266c8282bfc183598b5ea
icehongssii12345 <icehongssii@gmail.com>
2024-04-17 17:22:47 +0900

    Feat: 로그인 성공시 accesstoken발급 #2

--- users/router.py
+++ users/router.py
@@ -1,9 +1,25 @@
 from fastapi import APIRouter, Depends, HTTPException
+import json
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
 from sqlalchemy.orm import Session
 from . import schema, services
 from db import get_db
 
 router = APIRouter()
+OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="token")
+REFRESH_TKN_EXP_MIN = 1440
+ALGORITHM = "HS256"
+SECRET_KEY = "5d7253c2dc63339f8718bffdcd38302ed9e0bc8a8d4fc4bdca2d0be7f2de84ee1ad71189b36bc2b850c879983566def29453cb46417915831df074b89d44193f"
+ACCESS_TKN_EXP_MIN = 30
+
+def create_access_token(data: dict):
+    data['exp']  = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TKN_EXP_MIN)
+    data['mode'] = 'access'
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
 
 # /users/join
 @router.post("/join")
@@ -24,4 +40,9 @@ def login_user(user: schema.UserLogin, db:Session = Depends(get_db)):
     # 이메일 맞아도 비밀번호가 잘못되었으면 빡구
     if not services.verify_password(user.password.get_secret_value(), user_info.password):
         raise HTTPException(status_code=400, detail = "비밀번호가 틀렸어요")
-    return user_info.username
+    
+    # 이메일과 비밀번호가 잘 맞았다면 access token 발행
+    access_token = create_access_token(data={"email":user_info.email})
+    return access_token
+        
+    # 이메일과 비밀번호가 잘 맞았다면 refresh token 발행
