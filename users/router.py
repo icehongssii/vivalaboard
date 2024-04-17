99b7f7ee111e16c7041a144acf9cce9529c68d2c
icehongssii12345 <icehongssii@gmail.com>
2024-04-17 16:22:26 +0900

    Fix: 라우터 익셉션 statu code REVERT #3

--- users/router.py
+++ users/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from . import schema, services
 from db import get_db
@@ -8,4 +8,6 @@ router = APIRouter()
 # /users/join
 @router.post("/join")
 def create_user(user: schema.UserCreate, db:Session = Depends(get_db)):
-    return services.create_user(db, user)
+    if services.find_user_with_email(db, user.email):
+        raise HTTPException(status_code=400, detail = "같은 이메일 존재")     
+    return services.create_user(db,user)
