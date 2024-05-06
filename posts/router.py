8570d5ef38a33b43fa76500ba8114185dc5cc5bb
icehongssii12345 <icehongssii@gmail.com>
2024-05-06 18:39:02 +0900

    Hotfix: 작성글 수정시 유저확인 로직

--- posts/router.py
+++ posts/router.py
@@ -1,53 +1,54 @@
 from fastapi import APIRouter, Depends, Request, HTTPException
 from . import services, schema
-from typing import  List
+from typing import List
 from sqlalchemy.orm import Session
 from db import get_db
 router = APIRouter()
 
 
-"""삭제"""
 @router.get("/delete")
-def delete_post(req:Request,post_id:int, db:Session=Depends(get_db)):
-    post_user_id = services.get_writer_id(db,post_id).user_id
-    if not req.user:     
-        raise HTTPException(status_code=403, detail = "로그인하고오세요!")     
+def delete_post(req: Request, post_id: int, db: Session = Depends(get_db)):
+    post_user_id = services.get_writer_id(db, post_id).user_id
+    if not req.user:
+        raise HTTPException(status_code=403, detail="로그인하고오세요!")
     user_id = int(req.user)
     if user_id != post_user_id:
-        raise HTTPException(status_code=403, detail = "작성자만 수정할 수 있어요")     
-    print(services.delete_post(db,post_id),"삭제")
-    return {"result":"success", "deleted_post_id":post_id}
+        raise HTTPException(status_code=403, detail="작성자만 삭제 할 수 있어요")
+    return {"result": "success", "deleted_post_id": post_id}
 
-"""리스트"""
+
 @router.get("/", response_model=List[schema.PostResponse])
-def get_post_list(pagenation=Depends(schema.Pagination), db:Session = Depends(get_db)):
-    res = services.get_posts(db,pagenation)
-    return [schema.PostResponse(user_id=result.user_id, title=result.title, post_id=result.post_id,
-                                views=result.views, username=result.username) for result in res]
+def get_post_list(pagenation=Depends(schema.Pagination), db: Session = Depends(get_db)):
+    res = services.get_posts(db, pagenation)
+    return [schema.PostResponse(user_id=result.user_id, title=result.title,
+                                post_id=result.post_id, views=result.views,
+                                username=result.username)
+            for result in res]
 
-"""게시글 하나 보기"""
+
 @router.get("/view")
-def get_one_post(post_id:int=1, db:Session=Depends(get_db)):
+def get_one_post(post_id: int=1, db: Session=Depends(get_db)):
     return services.get_one_post(db, post_id)
 
 
-"""게시글 수정"""
 @router.post("/edit")
-def edit_post(req:Request,post:schema.PostEditReq, db:Session=Depends(get_db)):
+def edit_post(req: Request, post: schema.PostEditReq, db: Session=Depends(get_db)):
     # 실제로 클라이언트에서 요청을 보낼땐
     # post.user_id가 바뀔수가 없다
+    writer = services.get_writer_id(db, post.post_id).user_id
+    post.user_id = writer
     user_id = int(req.user)
-    if not user_id:     
-        raise HTTPException(status_code=403, detail = "로그인하고오세요!")     
+    if not user_id:
+        raise HTTPException(status_code=403, detail="로그인하고오세요!")
     if user_id != post.user_id:
-        raise HTTPException(status_code=403, detail = "작성자만 수정할 수 있어요")     
-    return services.edit_post(db,post)
+        raise HTTPException(status_code=403, detail="작성자만 수정할 수 있어요")
+    return services.edit_post(db, post)
 
-"""게시글 작성"""
+
 @router.post("/write")
-def write_post(req:Request,post:schema.PostWrite, db:Session=Depends(get_db)):
+def write_post(req: Request, post: schema.PostWrite, db: Session=Depends(get_db)):
     user_id = req.user
-    if not user_id:     
-        raise HTTPException(status_code=403, detail = "로그인하고오세요!")     
+    if not user_id:
+        raise HTTPException(status_code=403, detail="로그인하고오세요!")
     post.user_id = user_id
-    return services.create_post(db,post)
+    return services.create_post(db, post)
